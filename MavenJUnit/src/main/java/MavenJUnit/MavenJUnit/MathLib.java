dd6f5824fa033aa121954a866292d2d5b3433de8
malik-qasim <66017328+malik-qasim@users.noreply.github.com>
2024-11-15 10:08:02 +0500

    Add files via upload

---
+++ MavenJUnit/src/main/java/MavenJUnit/MavenJUnit/MathLib.java
@@ -0,0 +1,22 @@
+package MavenJUnit.MavenJUnit;
+
+
+public class MathLib 
+{
+	public int Add(int x, int y) {
+		return x+y;
+	}
+	
+	public double Add(double x, double y) {
+		return x+y;
+	}
+	
+	public double Mul(double x, double y) {
+		return x*y;
+	}
+	
+	public double Div(double x, double y) {
+		return x/y;
+	}
+    
+}
