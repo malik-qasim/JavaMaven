6592c44b6239ad7c80ee3a3cc317036a5f754d08
Qasim Malik <qasimalik@gmail.com>
2026-07-16 09:39:57 +0500

    Update TestWebApp.java

--- src/test/java/TestWebApp.java
+++ src/test/java/TestWebApp.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 class TestWebApp {
 	@Test
 	void testlogin() {
